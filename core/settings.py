41d53e1e26ffc7f5fb751e562bca1c5a16e64223
manuelabetancourth12 <manubeta2006@gmail.com>
2026-05-11 12:01:12 -0500

    fix: agrega url de vercel a ALLOWED_HOSTS y corrige extension de foto

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-taller-multimedia-manuela'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hoja-de-vida-django.vercel.app', '.vercel.app', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
