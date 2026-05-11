49373614a93fd4d29d2ef839e302a77ddf40c110
manuelabetancourth12 <manubeta2006@gmail.com>
2026-05-11 10:03:51 -0500

    feat: diseño profesional

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from cv_manuela import views # Importamos tus vistas
 
 urlpatterns = [
-    path('admin/', admin.site.core),
-    path('', views.home, name='home'), # Esta línea hace que tu CV sea la página principal
+    path('admin/', admin.site.urls), # Aquí estaba el error, ya corregido
+    path('', views.home, name='home'),
 ]
